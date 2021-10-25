0de6709ec200679346249eb7db46f941260c4fb3
hüseyin can soylu <hsyncn17@gmail.com>
2021-10-25 17:48:18 +0300

    added query logic

--- populate.js
+++ populate.js
@@ -13,9 +13,10 @@ const start = async () => {
 
     await Product.create(jsonProducts);
 
-    console.log("success!!");
+    process.exit(0);
   } catch (error) {
     console.log(error);
+    process.exit(1);
   }
 };
 
